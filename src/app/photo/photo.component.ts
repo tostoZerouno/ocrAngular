183efb72133fb72262d1c5d2c0a137a70914b6e6
tostoZerouno <tommaso.tosi92@outlook.it>
2017-03-15 16:35:32 +0100

    loop no prod

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -29,42 +29,61 @@ export class PhotoComponent implements OnInit {
     canvas.height = video.videoHeight;
     var context = canvas.getContext('2d');
     //canvas.getContext('2d').drawImage(video, 0, 0);
-    if (this.detectmob()) {
-      //var deg = Number(window.orientation);
-      context.save();
-      context.scale(1, -1);
-      context.drawImage(video, 0, 0, canvas.width, -1 * canvas.height);
-      context.restore();
+    var found = false;
+    var count = 0;
+    while (!found && count < 10) {
+      count++;
+      if (component.detectmob()) {
+        //var deg = Number(window.orientation);
+        context.save();
+        context.scale(1, -1);
+        context.drawImage(video, 0, 0, canvas.width, -1 * canvas.height);
+        context.restore();
 
-    } else {
-      context.drawImage(video, 0, 0);
+      } else {
+        context.drawImage(video, 0, 0);
+      }
+      const size = this.dataURItoBlob(canvas.toDataURL('image/jpeg', 1)).size;
+      var rapp = 4194304 / size;
+      this.log += size + " " + rapp;
+      rapp = Math.min(rapp, 1);
+      //console.log(size*rapp);
+      var image = canvas.toDataURL('image/jpeg', rapp);
+      const testCanvas = <any>document.getElementById('testCanvas');
+      var img = new Image;
+      img.src = image;
+      img.onload = function () {
+        //this.rettangolo.x, this.rettangolo.y, this.rettangolo.w, this.rettangolo.h
+        testCanvas.width = img.width * component.rettangolo.w;
+        testCanvas.height = img.height * component.rettangolo.h;
+        testCanvas.getContext('2d').drawImage(img, img.width * component.rettangolo.x, img.height * component.rettangolo.y,
+          img.width * component.rettangolo.w, img.height * component.rettangolo.h,
+          0, 0, img.width * component.rettangolo.w, img.height * component.rettangolo.h);
+
+        image = testCanvas.toDataURL('image/jpeg', 1);
+        //component.analyzeImage(image);
+        //Tesseract.SetRectangle(0,0,img.width*component.rettangolo.w, img.height*component.rettangolo.h);
+        Tesseract.recognize(image)
+          //.progress(function (p) { console.log('progress', p) })
+          .then(function (result) {
+            //console.log('result', result)
+            component.log = result.text;
+            var re = /([A-Z]{3})(I|\||l)([A-Z])(I|\||l)(I|\||l)(T|7)(I|\||l)([0-9]{4})(I|\||l)([0-9]{3})(I|\||l)([0-9]{8})/;
+            var out = re.test(result.text);
+            //console.log(out);
+            found = out;
+            
+          });
+      }
     }
-    const size = this.dataURItoBlob(canvas.toDataURL('image/jpeg', 1)).size;
-    var rapp = 4194304 / size;
-    this.log += size + " " + rapp;
-    rapp = Math.min(rapp,1);
-    //console.log(size*rapp);
-    var image = canvas.toDataURL('image/jpeg', rapp);
-    const testCanvas = <any>document.getElementById('testCanvas');
-    var img = new Image;
-    img.src = image;
-    img.onload = function () {
-      //this.rettangolo.x, this.rettangolo.y, this.rettangolo.w, this.rettangolo.h
-    testCanvas.getContext('2d').drawImage(img, img.width*component.rettangolo.x, img.height*component.rettangolo.y,
-     img.width*component.rettangolo.w, img.height*component.rettangolo.h,
-     0,0,img.width*component.rettangolo.w, img.height*component.rettangolo.h);
-    
-      image = testCanvas.toDataURL('image/jpeg',1);
-      component.analyzeImage(image);
-      //component.log= ""+window.orientation;
+    if(found){
+      this.log+="found";
+    }else{
+      this.log+="NOTfound";
     }
+
     //const vc = document.getElementById('videocomponent');
     //const mediastreamTrack = vc.localstream.getVideoTracks()[0];
-
-    /*Tesseract.recognize(image)
-         .progress(function  (p) { console.log('progress', p)    })
-         .then(function (result) { console.log('result', result) })*/
-
 
     //this.analyzeImage(image);
     //this.analyzeImage();
@@ -80,20 +99,20 @@ export class PhotoComponent implements OnInit {
     canvas.width = video.width;
     const ctx = canvas.getContext('2d');
 
-    this.orizzontale = ratio<1;
+    this.orizzontale = (ratio < 1);
     //console.log(this.orizzontale);
 
     ctx.strokeStyle = "#FF0000";
-    
-    if(this.orizzontale){
-      let rettangolo = {x : (1-1/1.8)/2, y: (1-1/10)/2, w: 1 / 1.8, h: 1 / 10};
+
+    if (this.orizzontale) {
+      let rettangolo = { x: (1 - 1 / 1.8) / 2, y: (1 - 1 / 10) / 2, w: 1 / 1.8, h: 1 / 10 };
       this.rettangolo = rettangolo;
-      
-    }else{
-      let rettangolo = {x : (1-1/10)/2, y:  (1-1/1.8)/2, w: 1 / 10, h: 1 / 1.8};
+
+    } else {
+      let rettangolo = { x: (1 - 1 / 10) / 2, y: (1 - 1 / 1.8) / 2, w: 1 / 10, h: 1 / 1.8 };
       this.rettangolo = rettangolo;
     }
-    ctx.strokeRect(video.width*this.rettangolo.x, video.height*this.rettangolo.y, video.width*this.rettangolo.w, video.height*this.rettangolo.h);
+    ctx.strokeRect(video.width * this.rettangolo.x, video.height * this.rettangolo.y, video.width * this.rettangolo.w, video.height * this.rettangolo.h);
 
 
     const tCanvas = <any>document.getElementById('testCanvas');
@@ -107,18 +126,18 @@ export class PhotoComponent implements OnInit {
     const blob = this.dataURItoBlob(stream);
 
     this.computerVision(blob).then(resp => {
-      this.log="";
+      this.log = "";
       const regions = resp['regions'];
-      if(regions.length>0){
+      if (regions.length > 0) {
         console.log(regions[0]);
         const r = regions[0].lines;
         var arr = Object.keys(r).map(function (key) { return r[key]; });
-        arr.forEach(element=>{
+        arr.forEach(element => {
           const words = element.words;
           var warr = Object.keys(words).map(function (key) { return words[key]; });
-          warr.forEach(w=>{
+          warr.forEach(w => {
             console.log(w.text);
-            this.log+=w.text+"\n";
+            this.log += w.text + "\n";
           })
 
         });
@@ -183,10 +202,7 @@ export class PhotoComponent implements OnInit {
     console.log(event);
     if (event == "stop") {
       this.enableCapture = false;
-    }/*else{
-      this.enableCapture=true;
-      this.evaluateAge();
-    }*/
+    }
   }
 
   clearCanvas() {
